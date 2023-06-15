eaca3431f417013b49e11ae5a7fe5e434ade0a92
Vartik Anand <vartikkanand@gmail.com>
2023-06-15 18:24:30 +0530

    tst

--- src/app/SubTopicExplanation/DetailExplanation/page.tsx
+++ src/app/SubTopicExplanation/DetailExplanation/page.tsx
@@ -13,14 +13,14 @@ const DetailExplanation: React.FC<DetailExplanationProps> = ({ Explanation }) =>
   return (
     <section className=''>
       <div className='grid  md:grid-cols-2 grid-cols-1 justify-start'>
-        <div className='flex gap-4 mb-3'>
+        {/* <div className='flex gap-4 mb-3'>
           <DetailExplanationBtn BtnName={'Q/A'} Click={handleClick} />
           <DetailExplanationBtn BtnName={'Simplify'} Click={handleClick} />
         </div>
         <div className='flex gap-2'>
           <DetailExplanationBtn BtnName={'Example'} Click={handleClick} />
           <DetailExplanationBtn BtnName={'Take Test'} Click={handleClick} />
-        </div>
+        </div> */}
       </div>
     </section>
   );
