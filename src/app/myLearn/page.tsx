db1376222253a8224b9871830f65b66f0bad6cff
Vartik Anand <vartikkanand@gmail.com>
2023-06-19 17:00:45 +0530

    soon

--- src/app/myLearn/page.tsx
+++ src/app/myLearn/page.tsx
@@ -1,15 +1,19 @@
 'use client'
+import Coding from '@/components/UnderCounstuction/Coding/Coding';
 import Sidebar from '@/components/sidebar/Sidebar';
 import React from 'react';
 
 const Page: React.FC = () => {
   return (
-    <div className="flex h-[100vh] md:hidden">
-      <Sidebar />
-      <div className="bg-green-300  flex-grow">
-        show page
-      </div>
+    <div>
+       <Coding/>
     </div>
+    // <div className="md:flex h-[100vh] hidden">
+    //   <Sidebar />
+    //   <div className="bg-green-300  flex-grow">
+    //     show page
+    //   </div>
+    // </div>
   );
 };
 
